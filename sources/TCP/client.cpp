845d927556e7c62a02da6b6b409c615dda958bf6
user <user@debian>
2015-12-15 00:13:18 +0300

    added authentication

--- sources/TCP/client.cpp
+++ sources/TCP/client.cpp
@@ -8,8 +8,13 @@
 #include <unistd.h>
 #include <string.h>
 
+#define bufSize 255
+
+int authentication(int sockfd);
+
 int main(int argc, char *argv[]) {
    int sockfd, portno, n;
+   int aut;
    struct sockaddr_in serv_addr;
    struct hostent *server;
    char quit[]="quit";
@@ -50,7 +55,10 @@ int main(int argc, char *argv[]) {
    /* Now ask for a message from the user, this message
       * will be read by server
    */
-   while (true){
+   do {
+           aut = authentication(sockfd); //процесс аутентификации клиента
+       } while (aut < 0);
+   while (1){
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
@@ -79,5 +87,75 @@ int main(int argc, char *argv[]) {
 
   }
     return 0;
+}
 
+
+int authentication(int sockfd){
+    char login[bufSize +1];
+    char reg[bufSize +1];
+    char buffer[bufSize +1];
+    char ok[]="ok";
+    char sign[]="sign_in";
+    char registration[]="register";
+    int n;
+    int res=-1;
+    bzero(buffer, bufSize+1);
+    printf("Sign in or register\n");
+    scanf("%s", &buffer);
+    if(strncmp(buffer,sign,sizeof(sign)-1) == 0){//вход существующего пользователя
+        printf("Enter your username:\n");
+        scanf("%s", &login);
+        n=write(sockfd,"exist",bufSize);//посылка серверу сообщения о том, что входит существующий пользователь
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+        n=write(sockfd,login,bufSize);//посылка серверу имени пользователя
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+        bzero(buffer,bufSize+1);
+        n = read(sockfd, buffer, bufSize);//ответ от сервера, правильны ли данные или нет
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+        if(strncmp(buffer,ok,sizeof(ok)-1) == 0){//данные правильны
+            printf("Hello %s, you has successfully logined\n",login);
+            res=1;
+        }
+        else{//данные не правильны
+            printf("No such username. Type correct username or register\n");
+            res=-1;
+        }
+    }
+    else if(strncmp(buffer,registration,sizeof(registration)-1) == 0){//регистрация нового пользователя
+        printf("Create new username:\n");
+        scanf("%s", &reg);
+        n=write(sockfd,"new",bufSize);//посылка сообщения серверу о регистрации нового пользователя
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+        n=write(sockfd,reg,bufSize);//посылка имени нового пользователя
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+        bzero(buffer,bufSize+1);
+        n = read(sockfd, buffer, bufSize);//ответ от сервера, правильны ли данные или нет
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+        if(strncmp(buffer,ok,sizeof(ok)-1) == 0){//данные правильны
+            printf("Hello %s, you has successfully registered and logined\n",reg);
+            res=1;
+        }
+        else{//данные не правильны
+            printf("User with this username is already existing. Create other username\n");
+        }
+     }
+    return res;
 }
--- sources/TCP/main.cpp
+++ sources/TCP/main.cpp
@@ -1,22 +1,28 @@
 #include <QCoreApplication>
+#include <pthread.h>
 #include <stdio.h>
+#include <cstdio>
 #include <stdlib.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <string>
 #include <unistd.h>
+#include <map>
 
 using namespace std;
 
-void doprocessing (int sock);
+#define bufSize 255
+
+void *doprocessing(void *sock);
+int authentication(int sock);
+int check_user(char buf[]);
 
 int main(int argc, char *argv[])
 {
     int sock, newsock, port_num, n;
     struct sockaddr_in serv_addr, cli_addr;
     socklen_t clilen;
-
     pid_t pid;
 
     sock=socket(AF_INET, SOCK_STREAM, 0);
@@ -40,9 +46,10 @@ int main(int argc, char *argv[])
 
     listen(sock,10);
     clilen=sizeof(cli_addr);
-
+    printf("TCP Server Waiting for client on port 12345\n");
 
     while (1) {
+         pthread_t thread;
          newsock = accept(sock, (struct sockaddr *) &cli_addr, &clilen);
 
          if (newsock < 0) {
@@ -50,48 +57,55 @@ int main(int argc, char *argv[])
             close(sock);
             exit(1);
          }
-
-         /* Create child process */
-         pid = fork();
-
-         if (pid < 0) {
-            perror("ERROR on fork");
-            exit(1);
+         void* tmp;
+         if (pthread_create(&thread, NULL, doprocessing, &newsock) != 0) {
+                     printf("Error while creating new thread\n");
+                     break;
          }
 
-         if (pid == 0) {
-            /* This is the client process */
-            close(sock);
-            while(true){
-                doprocessing(newsock);
-//              exit(0);
-               }
-         }
-         else {
-            close(newsock);
-         }
+//         /* Create child process */
+//         pid = fork();
+
+//         if (pid < 0) {
+//            perror("ERROR on fork");
+//            exit(1);
+//         }
+
+//         if (pid == 0) {
+//            /* This is the client process */
+//            close(sock);
+//            while(true){
+//                doprocessing(newsock);
+////              exit(0);
+//               }
+//         }
+//         else {
+//            close(newsock);
+//         }
       } /* end of while */
     return 0;
 
 }
 
-void doprocessing (int newsock) {
-   int n;
+
+
+void* doprocessing (void* newsock) {
+   int n, socket;
+   int aut;
    char buf[256];
    char buffer[256];
    char command[]="show_users";
    char quit[]="quit";
    bzero(buf,256);
    bzero(buffer,256);
-       n=recv(newsock, buf, 255, 0);
-       if (n < 0)
-           {
-             perror("ERROR reading from socket");
-             exit(1);
-           }
-
+   int *tmp=(int*)newsock;
+   socket=*tmp;
+   do {
+           aut = authentication(socket); //процесс аутентификации клиента
+       } while (aut < 0);
+   while(recv(socket, buf, 255, 0)>=0){
        if(strncmp(buf,quit,sizeof(quit)-1) == 0){
-               exit(1);
+               break;
        }
 
        if(strncmp(buf,command,sizeof(command)-1) == 0){
@@ -109,15 +123,135 @@ void doprocessing (int newsock) {
            }
            printf("\n");
            fclose(file);
-           n = send(newsock,buffer,sizeof(buffer), 0);
+           n = send(socket,buffer,sizeof(buffer), 0);
        }
        else{
            printf("No matches\n");
-           n = send(newsock,buf,sizeof(buf), 0);
+
+           n = send(socket,buf,sizeof(buf), 0);
        }
        if (n < 0)
        {
            perror("ERROR writing to socket");
            exit(1);
        }
+    }
+}
+
+int authentication(int sock){
+    char buf[bufSize+1];
+    char new_client[]="new";
+    char exist_client[]="exist";
+    int n,check;
+    int res;
+    bzero(buf, bufSize+1);
+    n=recv(sock, buf, bufSize, 0);//приём сообщения от клиента, в котором указано
+    //будет ли подключен новый пользователь или уже зарегистрированный
+    if (n < 0) {
+      perror("ERROR reading from socket");
+      exit(1);
+    }
+    if(strncmp(buf,exist_client,sizeof(exist_client)-1) == 0){
+        //уже существующий пользователь
+        bzero(buf,bufSize);
+        n=recv(sock, buf, bufSize, 0);
+        if (n < 0) {
+          perror("ERROR reading from socket");
+          exit(1);
+        }
+        check=check_user(buf);//проверка совпадает ли имя которое ввёл пользователь
+        //с именем в файле с пользователями
+        if (check < 0) {//нет совпадений
+            n = send(sock,"no_match",sizeof(buf), 0);//говорим клиенту, что нет совпадений
+            if (n < 0)
+            {
+                perror("ERROR writing to socket");
+                exit(1);
+            }
+            res=-1;
+        }
+        else{//есть совпадения
+            n = send(sock,"ok",sizeof(buf), 0);//говорим клиенту, что всё ок
+            if (n < 0)
+            {
+                perror("ERROR writing to socket");
+                exit(1);
+            }
+            res=1;
+            printf("Connected client %s\n",buf);
+        }
+    }
+    else if(strncmp(buf,new_client,sizeof(new_client)-1) == 0){
+        //создание нового пользователя
+        bzero(buf,bufSize);
+        n=recv(sock, buf, bufSize, 0);
+        if (n < 0) {
+          perror("ERROR reading from socket");
+          exit(1);
+        }
+        check=check_user(buf);//проверка нет ли уже такого имени у кого-нибудь
+        if(check>0){//есть совпадения
+            n = send(sock,"not_ok",bufSize, 0);//говорим клиенту, что не ок
+            if (n < 0)
+            {
+                perror("ERROR writing to socket");
+                exit(1);
+            }
+            res=-1;
+        }
+        else{//нет совпадений, клиент зарегистрирован
+            n = send(sock,"ok", bufSize, 0);//говорим клиенту, что всё ок
+            if (n < 0)
+            {
+                perror("ERROR writing to socket");
+                exit(1);
+            }
+            res=1;
+            printf("Connected client %s\n",buf);
+
+            FILE *file;
+            char *fname = "/home/user/project_t/us.txt";
+            file = fopen(fname,"a");
+            fprintf(file,buf);//запись в файл нового пользователя
+            fclose(file);
+        }
+    }
+    return res;
+}
+
+int check_user(char buf[]){
+    char name[bufSize+1];
+    char tmp[bufSize+1];
+    char id[10];
+    int res=-1;
+    int us_id,k;
+    FILE *file;
+    char *fname = "/home/user/project_t/us.txt";
+    file = fopen(fname,"r");
+    bzero(name,bufSize+1);
+    strcpy(name,buf);
+    if(file == NULL)
+        {
+            perror("ERROR on openning file with users");
+            exit(1);
+        }
+    int i = 0;
+    bzero(tmp,bufSize+1);
+    while(fscanf(file,"%s", tmp)!=EOF){
+        //fscanf(file,"%s", tmp);
+        if(k==0){
+            strcpy(id,tmp);
+            us_id=atoi(id);
+            printf("%d\n",us_id);
+            res=1;
+            break;
+        }
+        if(!(i%2)){
+            k=strcmp(name,tmp);
+            printf("%s\n",tmp);
+        }
+        i++;
+    }
+    fclose(file);
+    return res;
 }
